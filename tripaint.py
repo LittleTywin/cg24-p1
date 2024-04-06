f5ec59c05107c3d220ebefa18a0a9f133461e426
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-07 02:30:40 +0300

    copied f_shading to g_shading

--- tripaint.py
+++ tripaint.py
@@ -99,8 +99,78 @@ def f_shading(img, vertices, vcolors):
     return ret_img
 
 def g_shading(img, vertices, vcolors):
-    return f_shading(img,vertices,vcolors)
+    
+    """
+    Performs a triangle shading with the average color of its vertices and returns
+    the input image with the triangle rendered on top.
+    """
 
+    ret_img = np.copy(img)
+    triangle_color = np.average(vcolors,0)
+
+    sides = np.array([
+        [0,1],
+        [1,2],
+        [2,0],
+    ])
+
+    sides_x = vertices[:,0][sides]
+    sides_y = vertices[:,1][sides]
+    s_ind = np.argsort(sides_y,1)
+    sides_y_sorted = np.take_along_axis(sides_y,s_ind,1)
+    sides_x_sorted = np.take_along_axis(sides_x,s_ind,1)
+    sides_ymin = sides_y_sorted[:,0]
+    sides_ymax = sides_y_sorted[:,1]
+    sides_xmin = sides_x_sorted[:,0]
+    ymin = np.min(sides_ymin)
+    ymax = np.max(sides_ymax)
+
+    dx = vertices[:,0][sides[:,1]] - vertices[:,0][sides[:,0]]
+    dy = vertices[:,1][sides[:,1]] - vertices[:,1][sides[:,0]]
+    invm = dx/dy
+
+    horizontal_sides = np.isinf(invm)
+    point_sides = np.isnan(invm)
+    excluded_sides = np.logical_or(horizontal_sides,point_sides)
+    non_excluded_sides = np.logical_not(excluded_sides)
+    
+    active_sides = np.array([False,False,False])
+    active_sides = np.logical_and ((sides_ymin==ymin),non_excluded_sides)
+    border_points_x = sides_xmin.astype(np.float64)
+
+    for y in range(ymin,ymax+1):
+        active_border_points_x = border_points_x[active_sides]
+        try:
+            sorted_abpx = np.sort(active_border_points_x)
+            sorted_abpx = np.round(sorted_abpx).astype(np.int32)
+            for x in range(sorted_abpx[0],sorted_abpx[-1]):
+                ret_img[x,y] = triangle_color
+        except:
+            pass#print("no active border points")
+        new_active_sides = (sides_ymin == y+1)
+        removed_active_sides = (sides_ymax == y)
+        active_sides = np.logical_or(active_sides, new_active_sides)
+        active_sides = np.logical_and(active_sides,np.logical_not(removed_active_sides))
+        active_sides = np.logical_and(active_sides, non_excluded_sides)
+        active_border_points_x_calc_index = np.logical_and(active_sides, np.logical_not(new_active_sides))
+        border_points_x[active_border_points_x_calc_index] += invm[active_border_points_x_calc_index]
+        
+    #paint horizontal sides
+    if horizontal_sides.any():
+        for side in sides[horizontal_sides]:
+            x = vertices[:,0][side]
+            y = vertices[:,1][side]
+            for y in range(min(y),max(y)+1):
+                for x in range(min(x),max(x)+1):
+                    ret_img[x,y]=triangle_color
+    
+    #paint point sides
+    if point_sides.any():
+        for side in sides[point_sides]:
+            ret_img[vertices[side[0]][0],vertices[side[0]][1]]=triangle_color
+
+    return ret_img
+    
 def render_img(faces,vertices,vcolors,depth,shading):
     face_depth = np.average(depth[faces],1)
     s_ind = np.argsort(-face_depth)
