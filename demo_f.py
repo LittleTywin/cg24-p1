e8eca6c1d6549a4d205afaa7459b2037b6770de6
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-07 02:25:27 +0300

    added demp_f.py file

--- demo_f.py
+++ demo_f.py
@@ -1,8 +1,21 @@
 import numpy as np
+import os
+import tripaint as tp
+from matplotlib import pyplot as plot
 
-data = np.load("hw1.npy",allow_pickle=True).item(0)
-
-faces = data["faces"]
+#load data
+data = np.load("hw1.npy", allow_pickle=True).item(0)
 vertices = data["vertices"]
 vcolors = data["vcolors"]
-vdepth = data["depth"]
+depth = data["depth"]
+faces = data["faces"]
+
+
+flat_img = tp.render_img(faces, vertices, vcolors, depth,"flat")
+
+dir = "output_images"
+flat_file = "flat.png"
+path = os.path.join(os.getcwd(),dir)
+if not os.path.exists(path):
+    os.makedirs(path)
+plot.imsave(dir+'/'+flat_file,flat_img)
